f1fa3e5b827486bc596852cbd518cb848dbf7ad1
dabou <ch007m@gmail.com>
2017-07-11 15:08:46 +0200

    Use git url

--- generate_site.sh
+++ generate_site.sh
@@ -2,7 +2,7 @@
 
 rm -rf _site
 jekyll build --config _config_github.yml
-rm -rf gh-pages && git clone https://github.com/cmoulliard/site.git gh-pages
+rm -rf gh-pages && git clone git@github.com:cmoulliard/site.git gh-pages
 git config --global user.name "cmoulliard"
 git config --global user.email "ch007m@gmail.com"
 cd gh-pages && git checkout gh-pages
