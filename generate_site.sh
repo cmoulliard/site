f8588f80c0773f74cefe10c3a24d8036cdfe15e6
dabou <ch007m@gmail.com>
2017-07-11 15:09:40 +0200

    Move git config

--- generate_site.sh
+++ generate_site.sh
@@ -3,9 +3,9 @@
 rm -rf _site
 jekyll build --config _config_github.yml
 rm -rf gh-pages && git clone git@github.com:cmoulliard/site.git gh-pages
+cd gh-pages && git checkout gh-pages
 git config --global user.name "cmoulliard"
 git config --global user.email "ch007m@gmail.com"
-cd gh-pages && git checkout gh-pages
 git rm -rf .
 cp -r ../_site/ .
 git add .
